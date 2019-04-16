97a20915ea5560fdf10de5ad06239199412047c5
Peter Smart <iyiolapeter@gmail.com>
2019-04-16 11:43:12 +0100

    allow save to return file path

--- src/tests/main.ts
+++ src/tests/main.ts
@@ -1,9 +1,9 @@
-import { Workbook } from './../index';
+import { Workbook } from "./../index";
 
-const workbook = new Workbook('./','text.xlsx');
-const sheet = workbook.createSheet('test',2,2);
-sheet.set(1,1,'Yes');
-workbook.save().then(()=>{
-    // tslint:disable-next-line:no-console
-    console.log('done');
+const workbook = new Workbook("./", "text.xlsx");
+const sheet = workbook.createSheet("test", 2, 2);
+sheet.set(1, 1, "Yes");
+workbook.save().then(() => {
+  // tslint:disable-next-line:no-console
+  console.log("done");
 });
